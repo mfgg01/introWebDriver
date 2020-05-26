fc63502970d15b9034d49ea1883fca08717f462c
mfgg01 <mfgg01@yahoo.com.mx>
2020-05-25 22:04:13 -0500

    Ajuste al BuscarTrailer.java

--- src/classExercise/BuscarTrailer.java
+++ src/classExercise/BuscarTrailer.java
@@ -25,9 +25,9 @@ public class BuscarTrailer {
         // para cada parámetro
         navegarUrl("https://www.imdb.com/");
         verificarHomePage();
-        buscarCelebridad("Simon Baker");
-        //      verificarResultados (verificaResult: "Simon Baker (I)");
-        //     buscarTrailer();
+        buscarCelebridad("Celebs", textoBuscar: "Simon Baker");
+        verificarResultados();
+        correrTrailer();
     }
 
 
@@ -44,36 +44,40 @@ public class BuscarTrailer {
     }
 
 
-    private static void buscarCelebridad(String textoBuscar) {
+    private static void buscarCelebridad(String dropSeleccion, String textoBuscar) {
         //Declaro la variable "categoria" con su respectivo wait
-        //WebElement categoria= wait.until(ExpectedConditions.visibilityOfElementsLocated(By.cssSelector([class="ipc-button__text"]));
+        WebElement categoria= wait.until(ExpectedConditions.visibilityOfElementsLocated(By.cssSelector("[class='ipc-button__text']")));
+        categoria.sendKeys(dropSeleccion);
 
         //Declaro la variable "textBox" con su respectivo wait
         WebElement textBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("suggestion-search")));
         textBox.sendKeys(textoBuscar);
 
         //Declaro la variable "searchButton" con su respectivo wait
-        WebElement searchButton = driver.findElement(By.className("nav-search__search-submit _2wo2z_hfPCHWGjZvUGi_f1"));
+        WebElement searchButton = driver.findElement(By.className("nav-search__search-submit _2wo2z_hfPCHWGjZvUGi_f1")));
         searchButton.click();
     }
 }
 
-    /*private static void verificarResultados(    ) {
-        //Declaro la variable "loggedInMessage" de tipo "WebElement" con su respectivo wait para que aparezca el elemento
-        WebElement loggedInMessage = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".flash.notice")));
 
-        //Esta es la verificación para saber si el caso de prueba pasó o falló
-        //Si el texto de verificación es igual al texto que guarda la variable "loggedInMessage"
-        if  (loggedInMessage.getText().equals("The username or password you entered are incorrect"))
-            System.out.println("Caso de Prueba ejecutado correctamente"); //si es igual, el caso de prueba está OK
-        else
-            System.out.println("Caso de Prueba FALLADO"); //si no está igual, el caso de prueba está FAIL
-
-        driver.close(); //Para cerrar la ventana del navegador
-    }
+    private static void verificarResultados() {
+        WebElement resultLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.ByPartialLinkText("Simon Baker")));
+        resultLink.click();
+     }
 }
 
-    private static void buscarTrailer(String errorMessage) {
+
+    private static void correrTrailer() {
+        WebElement imageLink = wait.until(ExpectedCondtions.presenceOfElementLocated(By.ccsSelector("[class='wtw-option']")));
+        imageLink.click();
+        WebElement playButton = wait.until(ExpectedCondtions.presenceOfElementLocated(By.cssSelector("[class='slate_button prevent-ad-overlay video-modal']")));
+        playButton.click();
+        driver.manage().timeouts().implicitlyWait(159,  TimeUnit.SECONDS);
+
+        System.out.println("Nombre Trailer: " + nombreTrailer);
+        System.out.println("Duración en minutos: " + minutos);
+        System.out.println("Duración en segundos: " + segundos);
+
+        driver.close();
 
     }
-*/
